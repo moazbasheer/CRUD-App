f60171ad28252eb3c11083e581e280edc33be4df
moazbasheer <moazbasheer97@gmail.com>
2021-03-31 17:30:06 +0200

    add heroku config

--- pdo.php
+++ pdo.php
@@ -1,4 +1,12 @@
 <?php
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    var_dump($cleardb_url);
     $username='root';
     $pass='123456';
     $pdo = new PDO('mysql:host=localhost;dbname=misc',$username,$pass);
